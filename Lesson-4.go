1b83efa0928637de2b2fcfaa1a8c16e3c16fc835
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-13 16:01:06 +0300

    Update Lesson-4.go

--- Lesson-4.go
+++ Lesson-4.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	arr := []int{8, 5, 6, 4, 3, 8, 9, 1, 15, 51, 10}
-	Sort(arr)
-	fmt.Println(arr)
+	inputNums := []int64{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		//Отвечу почему сделал так.
+		//Сначала вводим несколько чисел (число и Enter).
+		//Затем, чтобы вывести отсортированный слайс, нажимаем еще раз Enter.
+		//Не смог разобраться как прервать набор массива и вывести результат в консоль, сделал вот так.
+		//Понимаю что нет вызова ошибок.
+		if err != nil {
+			Sort(inputNums)
+			fmt.Println(inputNums)
+			break
+		}
+		inputNums = append(inputNums, num)
+	}
 }
-
-func Sort(arr []int) {
+func Sort(arr []int64) {
 	for i := 1; i < len(arr); i++ {
 		a := arr[i]
 		b := i
