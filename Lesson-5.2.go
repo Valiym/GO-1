e815100f3f5b8a8fee22748b3b65938586ca7dc9
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-15 05:38:13 +0300

    Update Lesson-5.2.go

--- Lesson-5.2.go
+++ Lesson-5.2.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func fib(n int) int {
-	cash := make(map[int]int)
+	cache := make(map[int]int)
 	for i := 0; i <= n; i++ {
 		var f int
 		if i <= 2 {
 			f = 1
 		} else {
-			f = cash[i-1] + cash[i-2]
+			f = cache[i-1] + cache[i-2]
 		}
-		cash[i] = f
+		cache[i] = f
 	}
-	return cash[n]
+	return cache[n]
 }
 
 func main() {
