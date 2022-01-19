b8427b94e3f2edbd375315b71e812739953d5137
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-19 21:08:37 +0300

    Update Lesson-3.2.go

--- Lesson-3.2.go
+++ Lesson-3.2.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var number, j, i, flag int
+	var number, j, i int
+	var flag bool
 
 	fmt.Print("Введите число : ")
 	fmt.Scan(&number)
 	for j = 2; j <= number; j++ {
-		flag = 0
+		flag = false
 		for i = 2; i <= j/i; i++ {
 			if j%i == 0 {
-				flag = 1
+				flag = true
 				break
 			}
 		}
-		if flag == 0 {
+		if flag == false {
 			fmt.Println(j)
 		}
 	}
