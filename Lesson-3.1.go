02d4752dede486eee0fbc26d8511939e87a7f768
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-19 22:34:40 +0300

    Update Lesson-3.1.go

--- Lesson-3.1.go
+++ Lesson-3.1.go
@@ -6,10 +6,35 @@ import (
 	"os"
 )
 
+var a, b float64
+var err error
+var op string
+
+func sum(a, b float64, op string) (res float64) {
+	switch op {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		if b == 0 {
+			fmt.Println("Ошибка - Деление на ноль")
+			os.Exit(1)
+		}
+		res = a / b
+	case "%":
+		res = math.Mod(a, b)
+
+	default:
+		fmt.Println("Ошибка - Операция выбрана неверно")
+		os.Exit(1)
+	}
+	return
+}
+
 func main() {
-	var a, b, res float64
-	var op string
-	var err error
 	fmt.Print("Введите первое число: ")
 	_, err = fmt.Scanln(&a)
 	if err != nil {
@@ -24,27 +49,7 @@ func main() {
 	}
 	fmt.Print("Введите арифметическую операцию (+, -, *, /, %): ")
 	fmt.Scanln(&op)
-	switch op {
-	case "+":
-		res = a + b
-	case "-":
-		res = a - b
-	case "*":
-		res = a * b
-	case "/":
-		if b == 0 {
-			fmt.Println("Ошибка - Деление на ноль")
-			os.Exit(1)
-		}
-		res = a / b
 
-	case "%":
-		res = math.Mod(a, b)
-
-	default:
-		fmt.Println("Ошибка - Операция выбрана неверно")
-		os.Exit(1)
-	}
-	fmt.Printf("Результат выполнения операции: %f\n", res)
+	fmt.Printf("Результат выполнения операции: %f\n", sum(a, b, op))
 	return
 }
