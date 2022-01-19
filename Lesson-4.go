df11ae6a65a80adc95dbb5a4813c3248d6c6b372
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-19 23:09:32 +0300

    Update Lesson-4.go

--- Lesson-4.go
+++ Lesson-4.go
@@ -9,20 +9,16 @@ import (
 
 func main() {
 	inputNums := []int64{}
-	var flag bool
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		num, err := strconv.ParseInt(scanner.Text(), 10, 64)
 		if err != nil {
-			flag = true
-			break
+			panic(err)
 		}
 		inputNums = append(inputNums, num)
 	}
-	if flag == true {
-		Sort(inputNums)
-		fmt.Println(inputNums)
-	}
+	Sort(inputNums)
+	fmt.Println(inputNums)
 }
 func Sort(arr []int64) {
 	for i := 1; i < len(arr); i++ {
