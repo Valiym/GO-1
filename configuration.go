1f3b014dd28d1999ee56ee0529e8310b29fb375d
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-01-28 14:14:54 +0300

    Update configuration.go

--- configuration.go
+++ configuration.go
@@ -7,21 +7,21 @@ import (
 )
 
 type Configuration struct {
-	port        int    `envconfig:"PORT" default:"8080"`
-	dbUrl       string `envconfig:"DB_URL" default:"postgres: //db-user:db-password@petstore-db:5432/petstore?sslmode=disable"`
-	jaegerUrl   string `envconfig:"JAEGER_URL" default:"http: //jaeger:16686"`
-	sentryUrl   string `envconfig:"SENTRY_url" default:"http: //sentry:9000"`
-	kafkaBroker int    `envconfig:"kafka_broker" default:"kafka:9092"`
-	someAppId   string `envconfig:"some_appId" default:"testid"`
-	someAppKey  string `envconfig:"some_appKey" default:"testkey"`
+	Port        int    `envconfig:"PORT" default:"8080"`
+	DbUrl       string `envconfig:"DB_URL" default:"postgres: //db-user:db-password@petstore-db:5432/petstore?sslmode=disable"`
+	JaegerUrl   string `envconfig:"JAEGER_URL" default:"http: //jaeger:16686"`
+	SentryUrl   string `envconfig:"SENTRY_url" default:"http: //sentry:9000"`
+	KafkaBroker string `envconfig:"kafka_broker" default:"kafka:9092"`
+	SomeAppId   string `envconfig:"some_appId" default:"testid"`
+	SomeAppKey  string `envconfig:"some_appKey" default:"testkey"`
 }
 
-func Load() (*Configuration, error) { //, error
-	config := Configuration{}
+func Load() (Configuration, error) {
+	var config Configuration
 	err := envconfig.Process("configuration", &config)
 	if err != nil {
 		fmt.Println("config невозможно загрузить: %W", err)
 		os.Exit(1)
 	}
-	return &Configuration{port: 8181}, err
+	return config, err
 }
