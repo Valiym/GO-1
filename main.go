77f2860ff66510e69d31aa0dc1ae9418eb720dd1
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-02-04 23:53:02 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/Valiym/GO/Lesson-8/configuration"
-	"os"
 )
 
 func main() {
-
 	config, err := configuration.Load()
 	if err != nil {
-		fmt.Println("config could not be loade: %W", err)
-		os.Exit(1)
+		fmt.Println("config could not be load: ", err)
 	}
 	fmt.Println(config)
 }
