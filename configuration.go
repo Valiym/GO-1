a8be3c2405d71a00e2994e21c7f706ef7c413703
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-02-10 10:53:14 +0300

    Update configuration.go

--- configuration.go
+++ configuration.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/url"
@@ -10,64 +11,77 @@ import (
 
 type Configuration struct {
 	Port        int    `json:"port"`
-	DbUrl       string `json:"db_url"`
-	JaegerUrl   string `json:"jaeger_url"`
-	SentryUrl   string `json:"sentry_url"`
+	DbURL       string `json:"db_url"`
+	JaegerURL   string `json:"jaeger_url"`
+	SentryURL   string `json:"sentry_url"`
 	KafkaBroker string `json:"kafka_broker"`
-	SomeAppId   int    `json:"some_app_id"`
+	SomeAppID   int    `json:"some_app_id"`
 	SomeAppKey  string `json:"some_app_key"`
 }
 
-func Load() (Configuration, error) {
-	file, err := os.Open("configuration.json")
-	if err != nil {
-		log.Fatalf("Не могу открыть файл: %v", err)
-	}
+func Load() (*Configuration, error) {
+	var config = Configuration{}
+	var fileName = ""
 
-	defer func() {
-		err := file.Close()
+	flag.IntVar(&config.Port, "port", 8080, "port 1000-65535")
+	flag.StringVar(&config.DbURL, "dbURL", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "database url")
+	flag.StringVar(&config.JaegerURL, "jaegerURL", "http://jaeger:16686", "jaeger url")
+	flag.StringVar(&config.SentryURL, "sentryURL", "http://sentry:9000", "sentry url")
+	flag.StringVar(&config.KafkaBroker, "kafka", "http://kafka:9092", "kafka broker")
+	flag.IntVar(&config.SomeAppID, "someAppID", 5002, "some app id")
+	flag.StringVar(&config.SomeAppKey, "someAppKey", "someAppKey", "some app key")
+	flag.StringVar(&fileName, "filename", "", "json config file")
+	flag.Parse()
+
+	if fileName != "" {
+		file, err := os.Open(fileName)
 		if err != nil {
-			log.Printf("Не могу закрыть файл: %v", err)
+			return nil, fmt.Errorf("can't open file: %w", err)
 		}
-	}()
 
-	app := Configuration{}
+		defer func() {
+			err := file.Close()
+			if err != nil {
+				log.Fatalf("can't close file: %v", err)
+			}
+		}()
 
-	err = json.NewDecoder(file).Decode(&app)
+		err = json.NewDecoder(file).Decode(&config)
+		if err != nil {
+			return nil, fmt.Errorf("can't decode json file: %w", err)
+		}
+	}
+	err := validate(&config)
 	if err != nil {
-		log.Printf("Не могу декодировать json-файл в структуру: %v", err)
+		return &config, err
 	}
 
-	err = validate(app)
-	if err != nil {
-		fmt.Println("can't validate config", err)
-	}
-	return app, nil
+	return &config, nil
 }
 
-func validate(cfg Configuration) error {
+func validate(cfg *Configuration) error {
 	if cfg.Port > 65535 || cfg.Port < 1000 {
-		fmt.Println("invalid port value")
+		return fmt.Errorf("invalid port value")
 	}
-	if !valid(cfg.DbUrl) {
-		fmt.Println("invalid dbUrl value")
+	if !validURL(cfg.DbURL) {
+		return fmt.Errorf("invalid dbURL value")
 	}
-	if !valid(cfg.JaegerUrl) {
-		fmt.Println("invalid jaegerUrl value")
+	if !validURL(cfg.JaegerURL) {
+		return fmt.Errorf("invalid jaegerURL value")
 	}
-	if !valid(cfg.SentryUrl) {
-		fmt.Println("invalid sentryUrl value")
+	if !validURL(cfg.SentryURL) {
+		return fmt.Errorf("invalid sentryURL value")
 	}
-	if !valid(cfg.KafkaBroker) {
-		fmt.Println("invalid kafkaBroker value")
+	if !validURL(cfg.KafkaBroker) {
+		return fmt.Errorf("invalid kafkaBroker value")
 	}
-	if cfg.SomeAppId < 1000 {
-		fmt.Println("invalid someAppId value")
+	if cfg.SomeAppID < 1000 {
+		return fmt.Errorf("invalid someAppID value")
 	}
 	return nil
 }
 
-func valid(str string) bool {
+func validURL(str string) bool {
 	p, err := url.Parse(str)
 	return err == nil || p.Scheme != "" || p.Host != ""
 }
