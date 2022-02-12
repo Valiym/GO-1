157287690ab6add8441e3f190099d26c049e2945
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-02-12 22:10:26 +0300

    Lesson-10.2

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+var a, b float64
+var op string
+
+func Sum(a, b float64, op string) (res float64) {
+	switch op {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		if b == 0 {
+			fmt.Println("Ошибка - Деление на ноль")
+			os.Exit(1)
+		}
+		res = a / b
+	case "%":
+		res = math.Mod(a, b)
+
+	default:
+		fmt.Println("Ошибка - Операция выбрана неверно")
+		os.Exit(1)
+	}
+	return
+}
+
+func main() {
+	fmt.Print("Введите первое число: ")
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		fmt.Printf("Ошибка - Введите число")
+		return
+	}
+	fmt.Print("Введите второе число: ")
+	_, err = fmt.Scanln(&b)
+	if err != nil {
+		fmt.Printf("Ошибка - Введите число")
+		return
+	}
+	fmt.Print("Введите арифметическую операцию (+, -, *, /, %): ")
+	fmt.Scanln(&op)
+
+	fmt.Printf("Результат выполнения операции: %f\n", Sum(a, b, op))
+	return
+}
